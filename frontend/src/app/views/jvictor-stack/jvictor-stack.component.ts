e3da2b7aef99ecc24cfec144f0c3a1d70cefe4a9
jvictorhub <victorjtube@gmail.com>
2021-01-28 02:48:14 -0300

    ADDED responsive flexbox projects + stack + logos

--- frontend/src/app/views/jvictor-stack/jvictor-stack.component.ts
+++ frontend/src/app/views/jvictor-stack/jvictor-stack.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderService } from './../../components/template/nav/nav.service';
+import {
+  faYoutube, faLinkedin, faGithub, faJsSquare, faHtml5, faCss3,
+  faStackOverflow, faCodepen, faDiscord, faUbuntu
+} from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faFileAlt, faFutbol } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-jvictor-stack',
@@ -7,6 +12,21 @@ import { HeaderService } from './../../components/template/nav/nav.service';
   styleUrls: ['./jvictor-stack.component.css']
 })
 export class JvictorStackComponent implements OnInit {
+
+  faYoutube = faYoutube
+  faLinkedin = faLinkedin
+  faGithub = faGithub
+  faEnvelope = faEnvelope
+  faFileAlt = faFileAlt
+  faFutbol = faFutbol
+  faJsSquare = faJsSquare
+  faHtml5 = faHtml5
+  faCss3 = faCss3
+  faStackOverflow = faStackOverflow
+  faCodepen = faCodepen
+  faDiscord = faDiscord
+  faUbuntu = faUbuntu
+
 
   constructor(private headerService: HeaderService) {
     headerService.headerData = {
