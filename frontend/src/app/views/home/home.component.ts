d405a680a6dee1bb73e7f24320f5725091721c35
jvictorhub <victorjtube@gmail.com>
2021-01-30 02:29:37 -0300

    ADDED random quote from TG bot group in home page

--- frontend/src/app/views/home/home.component.ts
+++ frontend/src/app/views/home/home.component.ts
@@ -1,5 +1,7 @@
 import { HeaderService } from './../../components/template/nav/nav.service';
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +10,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private headerService: HeaderService) {
+  quote: any = { quote: '', author: '' };
+  randomQuoteURL = "https://script.google.com/macros/s/AKfycby9ZWcPcTWaTtvyCK8tv4pJXfe0-VZUo0vacJXF9JGx2xid8Xr2eEOy/exec?getQuotes=random"
+
+  constructor(private headerService: HeaderService, private http: HttpClient) {
     headerService.headerData = {
       title: 'Home',
       icon: 'home',
@@ -16,8 +21,22 @@ export class HomeComponent implements OnInit {
     }
   }
 
-
   ngOnInit(): void {
+    console.log('getRandomQuote()... ')
+    this.quote = this.getRandomQuote().subscribe(quote => {
+      this.quote = quote
+    })
+  }
+
+  getRandomQuote(): Observable<Object> {
+    return this.http.get<Object>(this.randomQuoteURL)
+  }
+
+  getNewRandomQuote(): void {
+    console.log('getRandomQuote()... ')
+    this.quote = this.getRandomQuote().subscribe(quote => {
+      this.quote = quote
+    })
   }
 
 }
