ba9bb4eb0ed2993277aea608f4df7ae00c23fac6
jvictorhub <victorjtube@gmail.com>
2021-02-02 22:26:45 -0300

    add target=_blank jvictor stack techs + minor fix

--- frontend/src/app/views/contact/contact.component.ts
+++ frontend/src/app/views/contact/contact.component.ts
@@ -1,5 +1,7 @@
 import { HeaderService } from './../../components/template/nav/nav.service';
 import { Component, OnInit } from '@angular/core';
+import { faYoutube, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faFileAlt, faFutbol } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-contact',
@@ -7,6 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  faYoutube = faYoutube
+  faLinkedin = faLinkedin
+  faGithub = faGithub
+  faEnvelope = faEnvelope
+  faFileAlt = faFileAlt
+
 
   constructor(private headerService: HeaderService) {
     headerService.headerData = {
